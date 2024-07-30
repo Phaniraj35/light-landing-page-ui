8f03ce29cfd88b4d10d102ecf7fafc7047a4f99c
phaniraj35 <phaniitng@gmail.com>
2024-07-30 18:18:05 +0530

    Popular tag animation added

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,7 +1,8 @@
+"use client";
 import { PricingCard } from '@/components/ui';
 import { twMerge } from 'tailwind-merge';
 import CheckIcon from '@/assets/check.svg';
-
+import { motion } from 'framer-motion';
 
 const pricingTiers = [
   {
@@ -71,9 +72,20 @@ export const Pricing = () => {
                 {
                     !!tier.popular && (
                       <div className="inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20">
-                          <span className='bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] text-transparent bg-clip-text font-medium'>
-                            Popular
-                          </span>
+                        <motion.span
+                          className='bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF)] [background-size:200%] text-transparent bg-clip-text font-medium'
+                          animate={{
+                            backgroundPositionX: '100%'
+                          }}
+                          transition={{
+                            repeat: Infinity,
+                            repeatType: 'loop',
+                            ease: 'linear',
+                            duration: 1
+                          }}
+                        >
+                          Popular
+                        </motion.span>
                       </div>
                     )
                 }
