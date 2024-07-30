f651acd6cef0cb08fec8ed61b3e8b439632655bd
phaniraj35 <phaniitng@gmail.com>
2024-07-30 19:09:51 +0530

    added CTA animations

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,29 +1,42 @@
+"use client";
 import ArrowRight from '@/assets/arrow-right.svg';
 import starImage from '@/assets/star.png';
 import springImage from '@/assets/spring.png';
-import Image from 'next/image';
+import { motion, useScroll, useTransform } from 'framer-motion';
+import { useRef } from 'react';
 
 export const CallToAction = () => {
+  const ctaSectionref = useRef<HTMLElement|null>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ctaSectionref,
+    offset: ["start end", "end start"]
+  });
+
+  const translateY = useTransform(scrollYProgress, [0,1], [-100, 100]);
+
   return (
-    <section className='bg-gradient-to-b from-white to-[#D2DCFF] pb-5 overflow-x-clip '>
+    <section ref={ctaSectionref} className='bg-gradient-to-b from-white to-[#D2DCFF] pb-5 pt-10 overflow-x-clip '>
       <div className="container relative">
       <h2 className="section-title">Sign up for free today</h2>
 
       <div className="container relative">
         <p className="section-description mt-5">Celebrate the joy of accomplishment with an app designed to track your progress and motivate your efforts.</p>
-          <Image 
-          src={starImage}
+          <motion.img 
+          src={starImage.src}
           alt='star graphic'
           width={120}
-          className='hidden md:block absolute -top-28 -left-10'
+          className='hidden md:block absolute bottom-0 -left-10'
+          style={{ translateY }}
         />
       </div>
 
-      <Image 
-        src={springImage}
+      <motion.img 
+        src={springImage.src}
         alt='spring graphic'
         width={120}
-        className='hidden md:block absolute right-10'
+        className='hidden md:block absolute right-10 bottom-10'
+        style={{ translateY }}
       />
 
       <div className="flex justify-center gap-2 items-center mt-10">
